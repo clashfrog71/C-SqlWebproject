28d68e01c4a2af94a680aeaecf4178d7bc3fdd70
Tiago Pereira <clashfrog71@gmail.com>
2025-05-20 20:29:12 -0300

    fazer conecxao com artistaDAL

--- ScreenSound/Banco/ArtistaDAL.cs
+++ ScreenSound/Banco/ArtistaDAL.cs
@@ -1,12 +1,34 @@
-﻿using System;
+﻿using ScreenSound.Modelos;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ScreenSound.Banco
+namespace ScreenSound.Banco;
+internal class ArtistaDAL
 {
-    internal class ArtistaDAL
+    public IEnumerable<Artista> Listar()
     {
+        var lista = new List<Artista>();
+        Conexao conexao = new Conexao();
+        using var connection = conexao.ObterConexao();
+        connection.Open();
+
+        string ComandoSql = "SELECT * FROM Artistas";
+        SqlCommand sql = new SqlCommand(ComandoSql, connection);
+        using SqlDataReader sqlDataReader = sql.ExecuteReader();
+
+        while (sqlDataReader.Read())
+        {
+            string nomeArtista = Convert.ToString(sqlDataReader["Nome"]);
+            string bioArtista = Convert.ToString(sqlDataReader["Bio"]);
+            int idArtista = Convert.ToInt32(sqlDataReader["Id"]);
+            Artista artista = new(nomeArtista, bioArtista) { Id = idArtista };
+            lista.Add(artista);
+        }
+        return lista;
+
     }
 }
--- ScreenSound/Banco/Conexao.cs
+++ ScreenSound/Banco/Conexao.cs
@@ -13,25 +13,5 @@ internal class Conexao
     {
         return new SqlConnection(connectionString);
     }
-    public IEnumerable<Artista> Listar()
-    {
-        var lista = new List<Artista>();
-        using var connection = ObterConexao();
-        connection.Open();
-
-        string ComandoSql = "SELECT * FROM Artistas";
-        SqlCommand sql = new SqlCommand(ComandoSql, connection);
-        using SqlDataReader sqlDataReader = sql.ExecuteReader();
-
-        while (sqlDataReader.Read())
-        {
-            string nomeArtista = Convert.ToString(sqlDataReader["Nome"]);
-            string bioArtista = Convert.ToString(sqlDataReader["Bio"]);
-            int idArtista = Convert.ToInt32(sqlDataReader["Id"]);
-            Artista artista = new(nomeArtista,bioArtista) { Id = idArtista };
-            lista.Add(artista);
-        }
-        return lista;
-
-    }
+    
 }
