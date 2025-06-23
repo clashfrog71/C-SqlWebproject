19d2a89374169768df890b319ede4522fcdfbee4
Tiago Pereira <clashfrog71@gmail.com>
2025-06-22 23:46:35 -0300

    fazer o proxies funcionar

--- ScreenSound/Modelos/Artista.cs
+++ ScreenSound/Modelos/Artista.cs
@@ -1,8 +1,8 @@
 ﻿namespace ScreenSound.Modelos; 
 
-internal class Artista 
+public class Artista 
 {
-    public ICollection<Musica> Musicas { get; set; } = new List<Musica>();
+    public virtual ICollection<Musica> Musicas { get; set; } = new List<Musica>();
 
     public Artista(string nome, string bio)
     {
